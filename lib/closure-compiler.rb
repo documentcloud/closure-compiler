475e1d8a79c1eb9478ef332c22bcfa8fa83408de
Jeremy Ashkenas <jashkenas@gmail.com>
2009-11-20 13:58:09 -0500

    Version 0.1.3, upgraded to the Closure Compiler 2009-11-19 release

--- lib/closure-compiler.rb
+++ lib/closure-compiler.rb
@@ -2,10 +2,12 @@ require File.expand_path(File.dirname(__FILE__) + '/closure/compiler')
 
 module Closure
 
-  VERSION = "0.1.2"
+  VERSION           = "0.1.3"
 
-  JAVA_COMMAND = 'java'
+  COMPILER_VERSION  = "20091119"
 
-  COMPILER_JAR = File.expand_path(File.dirname(__FILE__) + "/../vendor/closure-compiler-20091113.jar")
+  JAVA_COMMAND      = 'java'
+
+  COMPILER_JAR      = File.expand_path(File.dirname(__FILE__) + "/../vendor/closure-compiler-#{COMPILER_VERSION}.jar")
 
 end
