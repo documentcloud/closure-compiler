7a6880f2b3e1600575c36c98c1fb45bf5cd730d0
Jeremy Ashkenas <jashkenas@gmail.com>
2010-03-31 23:31:33 -0400

    Closure Compiler 0.2.1, with the latest version from Google (March 30, 2010)

--- lib/closure-compiler.rb
+++ lib/closure-compiler.rb
@@ -2,9 +2,9 @@ require File.expand_path(File.dirname(__FILE__) + '/closure/compiler')
 
 module Closure
 
-  VERSION           = "0.2.0"
+  VERSION           = "0.2.1"
 
-  COMPILER_VERSION  = "20100201"
+  COMPILER_VERSION  = "20100330"
 
   JAVA_COMMAND      = 'java'
 
